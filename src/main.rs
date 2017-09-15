3628584eb7fbc63fb8c26f124dee965b7a76ceb7
inv2004 <inv2004@gmail.com>
2017-09-15 03:03:19 -0400

    init upload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(test)]
+#![feature(type_ascription)]
+#[macro_use]
+
+extern crate lazy_static;
+extern crate test;
+extern crate rand;
+
+mod tests {
+	use rand::{thread_rng, Rng};
+	use test::Bencher;
+    lazy_static! {
+    	static ref VEC1: Vec<i64> = { (0..10_000_000).map(|_| thread_rng().gen_range(0,1000)).collect() };
+    }
+
+    // len: 10m ; q32: 65ms , rust: 41ms
+    #[bench] // find deltas
+    fn deltas_windows(b: &mut Bencher) {
+        b.iter(|| {
+            let r:Vec<i64> = VEC1.windows(2).map(|x| x[1] - x[0]).collect();
+            return r;
+        });
+    }
+
+    // let: 10m q32: 65ms , rust: 41ms
+    #[bench] // find deltas
+    fn deltas_zip(b: &mut Bencher) {
+        b.iter(|| {
+            let r:Vec<i64> = VEC1.iter().zip(VEC1.iter().skip(1)).map(|(x,y)| x-y).collect();
+            return r;
+        });
+    }
+
+    // let: 10m q32: 65ms , rust: 51ms
+    #[bench] // find deltas
+    fn deltas_iter(b: &mut Bencher) {
+        b.iter(|| {
+            let mut res:Vec<i64> = Vec::with_capacity(10_000_000);
+            for i in 0..(VEC1.len()-1) {
+                res.push(VEC1[i+1] - VEC1[i]);
+            }
+            return res;
+        });
+    }
+
 }
