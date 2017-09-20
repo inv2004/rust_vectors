cf5bf0b4825ced12593b72ae5fd0aaaa69dfecc8
inv2004 <inv2004@gmail.com>
2017-09-19 23:14:36 -0400

    main.rs removed old code

--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,3 @@ extern crate rand;
 // mod dev;
 mod wavg;
 
-//     // // let: 10m q32: 367ms , rust: 83ms
-//     // #[bench] // multiple odd numbers with 100
-//     // fn odd_mul_100_filter_map(b: &mut Bencher) {
-//     //     b.iter(|| {
-//     //         let r = VEC1.iter().filter(|x| *x%2 == 0).map(|x| x*100).collect::<Vec<i64>>();
-//     //         return r;
-//     //     });
-//     // }
-
-//     // // let: 10m q32: 367ms, rust: 39ms
-//     // #[bench] // multiple odd numbers with 100
-//     // fn odd_mul_100_map(b: &mut Bencher) {
-//     //     b.iter(|| {
-//     //         let r = VEC1.iter().map(|x| if x%2 == 0 { x*100 } else { *x }).collect::<Vec<i64>>();
-//     //         return r;
-//     //     });
-//     // }
-
-//     // // let: 10m q32: 367ms, rust: 39ms
-//     // #[bench] // multiple odd numbers with 100
-//     // fn odd_mul_100_for(b: &mut Bencher) {
-//     //     b.iter(|| {
-//     //         let mut a = VEC1;
-//     //         for i in a.iter_mut() {
-//     //             if *i%2 == 0 {
-//     //                 *i = *i*100;
-//     //             }
-//     //         }
-//     //         return VEC1;
-//     //     });
-//     // }
-
